f7a2a3c0d0e4078d07fdf479dfe916cf031e8472
Persan96 <per.sandgren@protonmail.ch>
2017-11-17 16:18:47 +0100

    Update convert_dec_to_frac.py
    Updated to take input direct from execution of script.

--- convert_dec_to_frac.py
+++ convert_dec_to_frac.py
@@ -4,9 +4,10 @@
 #import functions to check for decimal and gcd
 from decimal import Decimal
 from fractions import gcd
+import sys
 
-#Read in input from user
-dec = float(input("Insert decimal number: "))
+#Read in input from user into variable dec, set to float
+dec = float(sys.argv[1])
 #If input number is a decimal number, convert decimal into fraction, mod 1 on a decimal number will not return 0
 if(Decimal(dec) % 1 != 0): 
 	dec = int(dec*100)
