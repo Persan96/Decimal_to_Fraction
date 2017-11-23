c43254770e4b073233ca41bf7ac5d412ed3c1809
Persan96 <per.sandgren@protonmail.ch>
2017-11-23 09:05:56 +0100

    Update convert_dec_to_frac.py
    Commented out debugging

--- convert_dec_to_frac.py
+++ convert_dec_to_frac.py
@@ -9,16 +9,16 @@ import sys
 #Read in input from user into variable dec, set to float
 dec = float(sys.argv[1])
 tomulwith = int(len(sys.argv[1])-1)
-print("initial: ",dec, tomulwith)
+#print("initial: ",dec, tomulwith)
 #If input number is a decimal number, convert decimal into fraction, mod 1 on a decimal number will not return 0
 if(Decimal(dec) % 1 != 0): 
 	#get initial values multiplied with as many decimals there are
 	top = dec*10**tomulwith
 	bottom = 10**tomulwith
-	print("multiplication: ", top, bottom)
+	#print("multiplication: ", top, bottom)
 	#get gcd between top and bottom
 	saved_gcd = gcd(top, bottom)
-	print(saved_gcd)
+	#print(saved_gcd)
 	#Divide top and bottom with gcd
 	bottom = int(bottom/saved_gcd)
 	top = int(top/saved_gcd)
