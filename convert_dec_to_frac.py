944841b8baeaaba3cd5d8087501ea2c9d7d59b37
Persan96 <per.sandgren@protonmail.ch>
2017-11-23 09:02:25 +0100

    Update convert_dec_to_frac.py
    Addressed issue #1, now it works but 0.333333333333 generates a too long fraction

--- convert_dec_to_frac.py
+++ convert_dec_to_frac.py
@@ -8,12 +8,22 @@ import sys
 
 #Read in input from user into variable dec, set to float
 dec = float(sys.argv[1])
+tomulwith = int(len(sys.argv[1])-1)
+print("initial: ",dec, tomulwith)
 #If input number is a decimal number, convert decimal into fraction, mod 1 on a decimal number will not return 0
 if(Decimal(dec) % 1 != 0): 
-	dec = int(dec*10000)
-	saved_gcd = gcd(dec,10000)
-	dec = int(dec*0.001)
-	print(dec, "/", (saved_gcd))
+	#get initial values multiplied with as many decimals there are
+	top = dec*10**tomulwith
+	bottom = 10**tomulwith
+	print("multiplication: ", top, bottom)
+	#get gcd between top and bottom
+	saved_gcd = gcd(top, bottom)
+	print(saved_gcd)
+	#Divide top and bottom with gcd
+	bottom = int(bottom/saved_gcd)
+	top = int(top/saved_gcd)
+	#implement checker to simplify if too long number
+	print(top,"/",bottom)
 #If a non-decimal number has been set, just add /1 to make a fraction of it
 else:
 	print(int(dec),"/",1)
