41de49327dd6c12cc401e5693faa732024c65132
Persan96 <per.sandgren@protonmail.ch>
2017-11-28 11:05:04 +0100

    #3
    Fixes relating to #3

--- convert_dec_to_frac.py
+++ convert_dec_to_frac.py
@@ -1,29 +1,34 @@
-# Per Sandgren, Sweden, 17-11-2017
+# Per Sandgren, Sweden, 28-11-2017
 # Small program converting decimals to fractions
 
-#import functions to check for decimal and gcd
+#import functions to check for decimal
 from decimal import Decimal
-from fractions import gcd
 import sys
 
-#Read in input from user into variable dec, set to float
+#implementation of gcd
+def gcd(a,b) :
+	return_gcd = a
+	rest = a % b
+	if(rest != 0):
+		return_gcd = gcd(a,rest)
+	return return_gcd
+
+#Read in input from user into variable dec, short for decimal, set to float
 dec = float(sys.argv[1])
-tomulwith = int(len(sys.argv[1])-1)
-#print("initial: ",dec, tomulwith)
+to_mul_with = int(len(sys.argv[1])-1)
 #If input number is a decimal number, convert decimal into fraction, mod 1 on a decimal number will not return 0
 if(Decimal(dec) % 1 != 0): 
 	#get initial values multiplied with as many decimals there are
-	top = dec*10**tomulwith
-	bottom = 10**tomulwith
-	#print("multiplication: ", top, bottom)
+	top = dec*10**to_mul_with
+	bottom = 10**to_mul_with
 	#get gcd between top and bottom
 	saved_gcd = gcd(top, bottom)
-	#print(saved_gcd)
 	#Divide top and bottom with gcd
 	bottom = int(bottom/saved_gcd)
 	top = int(top/saved_gcd)
-	#implement checker to simplify if too long number
+	#print output fraction
 	print(top,"/",bottom)
+
 #If a non-decimal number has been set, just add /1 to make a fraction of it
 else:
 	print(int(dec),"/",1)
